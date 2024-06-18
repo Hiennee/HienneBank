75ea720090ee67a2f1c9f41deac32ad7519511ee
Hiennee <chanhien55@gmail.com>
2024-06-19 00:38:16 +0700

    Add loading screen while fetching

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
+import { ActivityIndicator, View, Alert } from 'react-native';
+import { useState } from 'react';
 import Main from './components/Main';
+import { IPAddr } from './shared/localIP';
+import { Text } from '@rneui/themed';
 
 export default function App() {
+  var [ isLoading, setIsLoading ] = useState(true);
+  fetch(IPAddr + "ping")
+  .then((respond) => {
+    if (respond.status == 234) setIsLoading(false)
+    else throw new Error("Error connecting to server");
+  })
+  .catch((err) => {
+    Alert.alert("THÔNG BÁO", "Có lỗi khi kết nối đến máy chủ", [
+      {text: "Kết nối lại"}
+    ], { cancelable: true })
+  })
+  console.log(isLoading)
   return ( // Nếu wrap <Main/> vào <View> </View> thì cái View trắng sẽ override Main?
-      <Main/>
+      <>
+        { isLoading ? <LoadingScreen/> : 
+        <Main /> }
+      </> 
   );
 }
 
+function LoadingScreen()
+{
+  return (
+    <View style={{justifyContent: 'center', flex: 1}}>
+      <ActivityIndicator size="large"/>
+      <Text style={{ textAlign: "center" }}>Đang tải...</Text>
+    </View>
+  )
+}
 
 
 
--- shared/localIP.js
+++ shared/localIP.js
@@ -1,1 +1,1 @@
-export const IPAddr = "http://192.168.1.5:8080/"
+export const IPAddr = "https://hiennebank.onrender.com/"
